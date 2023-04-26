d4330b7b2fdf19d68d6a028ed513332cb56b5f17
Vlad180900 <khursik.v@yandex.ru>
2023-04-26 16:43:39 +0300

    init

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,13 @@
+import com.java.DataType;
+
 public class Main {
     public static void main(String[] args) {
         System.out.printf("Hello and welcome!");
-        for (int i = 1; i <= 50; i++) {
-            System.out.println("i = " + i);
+        int last = 1;
+        for (int i = 1; i <= 20; i++) {
+            System.out.println(DataType.sum(i, )i * i - 1);
+//            last = DataType.sum(i, last);
+
         }
     }
 }
--- src/com/java/DataType.java
+++ src/com/java/DataType.java
@@ -1,4 +1,8 @@
 package com.java;
 
 public class DataType {
+    public static int  sum(int a, int c){
+        System.out.println("a = " + a + "; c =" + c);
+        return a * c;
+    }
 }
