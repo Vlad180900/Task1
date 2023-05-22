babbe73bfadcf0839c19bf8e3af4b27e2e0a69b6
Vlad180900 <khursik.v@yandex.ru>
2023-05-22 18:54:01 +0300

    Task4

--- src/task/Task4.java
+++ src/task/Task4.java
@@ -4,18 +4,19 @@ import java.util.Arrays;
 
     public class Task4 {
     public static void main(String[] args) {
-        int[] array = {1, 2, 3};
-
-        int[] reversedArray = reverse(array);
-        System.out.println("Reversed array : " + Arrays.toString(reversedArray));
-    }
-
-    static int[] reverse(int[] array) {
-        int[] newArray = new int[array.length];
+        int[] array = {1, 2, 3, 4};
         for (int i = 0; i < array.length; i++) {
-            newArray[array.length - 1 - i] = array[i];
+            System.out.print(array[i] + " ");
         }
-        return newArray;
+        int newArray = array.length;
+        for (int i = 0; i < array.length / 2; i++) {
+         int temp = array[newArray - i - 1];
+         array[newArray - i -1] = array[i];
+         array[i] = temp;
+        }
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
     }
 }
 
